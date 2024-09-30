1b72e9866be1c1cd2ed6bccfb20fc2f2d84fca1c
Jakob Larsen <jbla20@student.aau.dk>
2024-09-30 21:44:21 +0200

    Added DSO algorithm

---
+++ scripts/okvis/run_okvis.sh
@@ -0,0 +1,21 @@
+# Set up logging
+log_file="$(rospack find rudpt_evaluation)/logs/run_okvis.log"
+
+package_directory="$(rospack find rudpt_evaluation)"
+workspace_directory="$(catkin locate --src)/.."
+
+algorithm_name="okvis"
+input_directory="${1:-"$workspace_directory/test/rudpt/1,2"}"
+output_directory="$package_directory/eval_data/$algorithm_name"
+save_directory="${2:-"/storage/data/results/okvis/1,2"}"
+comment="${3:-""}"
+script -c "bash $package_directory/scripts/run.sh \
+                \"$input_directory\" \
+                \"$output_directory\" \
+                \"$algorithm_name\" \
+                \"$save_directory\" \
+                \"$comment\"" "$log_file"
+
+# Post-process the log file to remove unwanted characters
+col -b < "$log_file" > "${log_file}.tmp"
+mv "${log_file}.tmp" "$log_file"
